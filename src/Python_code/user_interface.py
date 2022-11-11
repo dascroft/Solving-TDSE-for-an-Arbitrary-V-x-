62a0aab257ac348e4f4ed96c2eafa9b47622f209
Wilson <ZHAP028@live.rhul.ac.uk>
2022-11-11 12:23:48 +0000

    Adding user input interface

--- src/Python_code/user_interface.py
+++ src/Python_code/user_interface.py
@@ -1,1 +1,5 @@
-potential_choice = input("Do you want to input a potential, or use the default ")
+input_yn = input("Do you want to input a potential? (y/n)  ")
+if input_yn.lower()=="y":
+    file_or_command_line = input("Do you have a text file (.txt) with your potential? (y/n) ")
+else:
+    print("DEFAULT POTENTIAL")
